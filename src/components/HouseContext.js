f1b048838b2784e6f0c2947557d972a0f67d4ad5
masudfcs1 <masudfcs1@gmail.com>
2023-05-17 15:54:38 +0600

    Use Usestate

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { createContext, useState } from "react";
+import { housesData } from "../data";
+export const HouseContext = createContext();
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+const HouseContextProvider = ({ children }) => {
+  const [houses, sethouses] = useState(housesData);
+  const [country, setcountry] = useState("Location (any)");
+  const [countries, setcountries] = useState([]);
+  const [property, setproperty] = useState("Property type()");
+  const [properties, setproperties] = useState([]);
+  const [price, setprice] = useState("price range (any)");
+  const [loading, setloading] = useState(false);
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setcountry,
+        countries,
+        property,
+        setproperty,
+        properties,
+        price,
+        setprice,
+        houses,
+        loading,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
