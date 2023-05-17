e3c3be9b9bfe49b4953a7e0fc073ecd20dcfce10
masudfcs1 <masudfcs1@gmail.com>
2023-05-17 13:57:57 +0600

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,14 +4,17 @@ import CountryDropdown from "./CountryDropdown";
 import PriceRangeDropdown from "./PriceRangeDropdown";
 import PropertyDropdown from "./PropertyDropdown";
 
-import { Ri24HoursLine } from "react-icons/ri";
+import { RiSearch2Line } from "react-icons/ri";
 
 const Search = () => {
   return (
-    <div>
+    <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-white lg:backdrop:blur rounded-lg  ">
       <CountryDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
+      <button className=" bg-violet-600 hover:bg-violet-800 transition w-full lg:max-w-[162px] h-16 rounded-lg flex justify-center items-center text-white text-2xl  ">
+        <RiSearch2Line />
+      </button>
     </div>
   );
 };
