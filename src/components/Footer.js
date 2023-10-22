48e19db3c890571ea9a4defe956d4fc57eb62de9
masud rana <masudfcs1@gmail.com>
2023-10-22 08:37:51 +0600

    Top house

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className=" bg-black py-8 text-center text-white">
       <div className=" container mx-auto">
         {" "}
-        Copyright & copy; 2023. All rights reserved.{" "}
+        <div> Copyright & copy; 2023. All rights reserved. </div>{" "}
+        <div>masudfcs1 </div>{" "}
       </div>{" "}
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/img/logo.svg";
+import Logo from "../assets/img/logo.png";
 
 const Header = () => {
   return (
